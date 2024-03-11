51fd4acd5ee595d4f4bfc3b383994f60fb56fdbe
Nang Khong Kham <n.khongkham0011@gmail.com>
2024-03-11 19:04:29 +0800

    Initial Commit

---
+++ frontend/src/app/about/page.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function AboutPage() {
+    return (
+        <main className={styles.main}>
+            <div>
+                <h1>This is About Page</h1>
+            </div>
+        </main>
+    );
+}
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return <Link href="/about">About Navigation</Link>;
+}

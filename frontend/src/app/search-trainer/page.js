7e6099bc8d45953bfa28f6a22ceb7345c16871bb
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-03-20 14:37:53 +0800

    Search Bar done

---
+++ frontend/src/app/search-trainer/page.js
@@ -0,0 +1,12 @@
+'use client'
+import { Box } from '@chakra-ui/react'
+import SearchBar from './../../components/searchBar';
+
+
+export default function SearchTrainer() {
+  return (
+    <Box>
+      <SearchBar/>
+    </Box>
+  )
+}
---
+++ frontend/src/components/searchBar.js
@@ -0,0 +1,28 @@
+'use client'
+
+import {
+  Box, Input, InputGroup, Button, InputRightElement
+} from '@chakra-ui/react'
+
+import { SearchIcon } from '@chakra-ui/icons';
+
+function SearchBar() {
+  return (
+    <Box maxWidth="400px">
+        <InputGroup size='md'>
+            <Input
+                pr='4.5rem'
+                placeholder=''
+                borderRadius={20}
+            />
+            <InputRightElement width='3rem' bg='inherit'>
+                <Button h='1.75rem' size='sm' borderRadius={30}>
+                    <SearchIcon/>
+                </Button>
+            </InputRightElement>
+        </InputGroup>
+    </Box>
+  )
+}
+
+export default SearchBar;

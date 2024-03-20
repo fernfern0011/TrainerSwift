eb492d4fad8566e7f9793717fb2dfa923122097d
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-03-21 00:18:19 +0800

    View-Trainer-Page done

---
+++ frontend/src/app/(trainee)/view-trainer-profile/page.js
@@ -0,0 +1,122 @@
+'use client'
+
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  VisuallyHidden,
+  List,
+  ListItem,
+} from '@chakra-ui/react'
+import { MdLocalShipping } from 'react-icons/md'
+
+export default function Simple() {
+  return (
+    <Container maxW={'7xl'}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Image
+            rounded={'md'}
+            alt={'product image'}
+            src={
+              'https://images.unsplash.com/photo-1596516109370-29001ec8ec36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE1MDl8MHwxfGFsbHx8fHx8fHx8fDE2Mzg5MzY2MzE&ixlib=rb-1.2.1&q=80&w=1080'
+            }
+            fit={'cover'}
+            align={'center'}
+            w={'100%'}
+            h={{ base: '120%', sm: '400px', lg: '550px' }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={'header'}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+              [Trainer Name]
+            </Heading>
+            <Text
+              color={useColorModeValue('gray.900', 'gray.400')}
+              fontWeight={300}
+              fontSize={'2xl'}>
+              [EMAIL]
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}
+            divider={
+              <StackDivider borderColor={useColorModeValue('gray.200', 'gray.600')} />
+            }>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                color={useColorModeValue('gray.500', 'gray.400')}
+                fontSize={'2xl'}
+                fontWeight={'300'}>
+                [AGE:]
+                [HEIGHT:]
+                [WEIGHT:]
+              </Text>
+              <Text fontSize={'lg'}>
+                Hi! I am John Doe! Your fitness trainer who will help you achieve your fitness goals. I love to play sports and video games (who doesnt?)
+              </Text>
+            </VStack>
+            <Box>
+              <Text
+                fontSize={{ base: '16px', lg: '18px' }}
+                color={useColorModeValue('yellow.500', 'yellow.300')}
+                fontWeight={'500'}
+                textTransform={'uppercase'}
+                mb={'4'}>
+                Expertise
+              </Text>
+
+              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <List spacing={2}>
+                  <ListItem>Calisthenics</ListItem>
+                  <ListItem>Bodybuilding</ListItem>{' '}
+                  <ListItem>Zumba</ListItem>
+                </List>
+                <List spacing={2}>
+                  <ListItem>Test</ListItem>
+                  <ListItem>Test</ListItem>
+                  <ListItem>Test</ListItem>
+                </List>
+              </SimpleGrid>
+            </Box>
+          </Stack>
+
+          <Button
+            rounded={'none'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+            Check out Packages!
+          </Button>
+        </Stack>
+      </SimpleGrid>
+    </Container>
+  )
+}

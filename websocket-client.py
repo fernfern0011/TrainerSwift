4346e45794f95f09b3581342dc8a77f8a9f35d44
melodywong23 <melody.wong.2022@scis.smu.edu.sg>
2024-03-21 23:03:20 +0800

    WORKING DOCKERISED WEBSOCKET OMG
    I FINALLY DID IT EVERYTHING WORKS OMG
    
    DISCLAIMER: YOU CAN'T REALLY SEE THE WEBSOCKETS COMMUNICATE W EACH OTHER IN DOCKER LOGS BUT YOU CAN SEE IT IN MONGODB ATLAS SO PLS TELL ME IF YOU WANT TO SEE IT WORKING AND I'LL SHOW YOU :heart: :heart: :heart: :heart_eyes: :heart_eyes:

--- websocket-client.py
+++ websocket-client.py
@@ -2,6 +2,8 @@ import asyncio
 import websockets
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+import json
 
 app=Flask(__name__)
 CORS(app)
@@ -9,24 +11,33 @@ CORS(app)
 @app.route('/websocketchat', methods=['POST'])
 def websocketchat():
     data = request.get_json()
+    print(data)
 
     #create connection + send message
     async def connect(message):
-        async with websockets.connect("ws://localhost:8765") as websocket:
+        async with websockets.connect("ws://websocket-server:8765") as websocket:
             await websocket.send(message)
             response = await websocket.recv()
             print("Received", repr(response))
-
+            chat_history = await websocket.recv()
+            if (chat_history):
+                parsed_data = json.loads(chat_history)
+                print("Received chat history")
+                for doc in parsed_data:
+                    print(doc)
     #test 
     async def main():
+        nonlocal data
         if (data["connection"] == True):
-            await connect(data["traineeid"], "connection created")
-            await connect(data["trainerid"],"connection created")
+            print(data)
+            await connect([data["traineeID"]+","+ "connection created"])
+            await connect([data["trainerID"]+","+  "connection created"])
         else:
-            await connect(data["traineeid"], "test message")
-            await connect(data["trainerid"], "test message")
+            await connect([data["traineeID"]+","+  "test message"])
+            await connect([data["trainerID"]+","+  "test message"])
 
     asyncio.run(main())
+    return jsonify({"response": "success"})
 
 if __name__ == "__main__":
     print("This is flask " + os.path.basename(__file__) + " for creating websocket.")

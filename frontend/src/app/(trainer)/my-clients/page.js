b128cfa12eec8b33fd1223131b110f810253fdd6
theMarcokuok <marcokuok6@gmail.com>
2024-04-03 00:43:44 +0800

    added session for trainer's my-clients

--- frontend/src/app/(trainer)/my-clients/page.js
+++ frontend/src/app/(trainer)/my-clients/page.js
@@ -3,22 +3,52 @@ import { useState, useEffect } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import ClientCard from '../../../components/clientCard';
 import SearchBar from '../../../components/searchBar';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 export default function MyClientPage() {
     const [bookings, setBookings] = useState([]);
     const [traineeDetails, setTraineeDetails] = useState([]);
+    const [checkToken, setCheckToken] = useState('');
+    const router = useRouter();
 
     useEffect(() => {
+        const token = Cookies.get('token')
+        const trainerinfo = Cookies.get('trainerinfo')
+        var trainerid
+
+        if (!token) {
+            router.replace('/') // If no token is found, redirect to login page
+            return
+        }
+
+        if (!(trainerinfo === undefined)) {
+            trainerid = JSON.parse(trainerinfo)
+        }
+
+        setCheckToken(token)
         const fetchBookings = async () => {
             try {
-                const responseBookings = await fetch('http://localhost:3000/api/trainer-booking/1');
+                const responseBookings = await fetch(`http://localhost:3000/api/trainer-booking/${trainerid.trainerid}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`,
+                    }
+                });
                 if (!responseBookings.ok) {
                     throw new Error('Failed to fetch bookings');
                 }
                 const dataBookings = await responseBookings.json();
                 setBookings(dataBookings.data.bookedby);
 
-                const responseTraineeDetails = await fetch('http://localhost:3000/api/trainer-details/1');
+                const responseTraineeDetails = await fetch(`http://localhost:3000/api/trainer-details/${trainerid.trainerid}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`,
+                    }
+                });
                 if (!responseTraineeDetails.ok) {
                     throw new Error('Failed to fetch trainee details');
                 }

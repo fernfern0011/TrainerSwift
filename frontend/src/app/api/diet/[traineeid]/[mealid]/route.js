294e9fce66300d2adb3ddcfad6c23434882dbe60
theMarcokuok <marcokuok6@gmail.com>
2024-04-02 21:36:48 +0800

    added session for trainer and diet api

--- frontend/src/app/api/diet/[traineeid]/[mealid]/route.js
+++ frontend/src/app/api/diet/[traineeid]/[mealid]/route.js
@@ -1,4 +1,6 @@
-import { NextResponse } from "next/server";
+import jwt from 'jsonwebtoken'
+import { NextResponse } from 'next/server'
+import { headers } from 'next/headers';
 
 const DATA_SOURCE_URL = 'http://localhost:8000/checkingdietapi/check_my_diet'
 
@@ -25,16 +27,23 @@ export async function DELETE(req, context) {
     };
 
     try {
-        const response = await fetch(DATA_SOURCE_URL, requestOptions); // Send POST request to the correct endpoint
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        return NextResponse.json(data);
+        const headersInstance = headers()
+        const authHeader = headersInstance.get('Authorization')
 
+        const token = authHeader.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decoded) {
+            return NextResponse.json({ "code": 400, "message": "Expired" })
+        } else if (decoded.exp < Math.floor(Date.now() / 1000)) {
+            return NextResponse.json({ "code": 400, "message": "Expired" })
+        } else {
+            const response = await fetch(DATA_SOURCE_URL, requestOptions); // Send POST request to the correct endpoint
+            const data = await response.json();
+            return NextResponse.json(data);
+        }
     } catch (error) {
-        console.error('Error:', error);
-        // Handle error appropriately
-        return NextResponse.error(error.message, { status: 500 });
+        console.error('Token verification failed', error)
+        return NextResponse.json({ "code": 400, "message": "Unauthorized" })
     }
 }

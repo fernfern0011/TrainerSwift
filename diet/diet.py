9769d6f88f60c3e2cc36011430a0aba3f1d3045a
theMarcokuok <marcokuok6@gmail.com>
2024-04-02 17:11:34 +0800

    edited diet.py

--- diet/diet.py
+++ diet/diet.py
@@ -22,10 +22,7 @@ def get_all_meal(traineeid):
     cur.close()
     con.close()
 
-    if meallist == None:
-        meallist_json = [{"traineeid": "No Data", "mealid": "No Data", "foodname": "No Data", "quantity": "No Data", "calories": "No Data", "carbs": "No Data", "protein": "No Data", "fat": "No Data", "date_time": "No Data"}]
-    else:
-        meallist_json = [{"traineeid": meal[0], "mealid": meal[1], "foodname": meal[2], "quantity": meal[3], "calories": round(meal[4],2), "carbs": round(meal[5],2), "protein": round(meal[6],2), "fat": round(meal[7],2), "date_time": meal[8]} for meal in meallist]
+    meallist_json = [{"traineeid": meal[0], "mealid": meal[1], "foodname": meal[2], "quantity": meal[3], "calories": round(meal[4],2), "carbs": round(meal[5],2), "protein": round(meal[6],2), "fat": round(meal[7],2), "date_time": meal[8]} for meal in meallist]
 
     if len(meallist):
         return jsonify({
@@ -37,7 +34,7 @@ def get_all_meal(traineeid):
     
     return jsonify({
         "code": 400,
-        "message": "There is no meal."
+        "message": [{"traineeid": "No Data", "mealid": "No Data", "foodname": "No Data", "quantity": "No Data", "calories": "No Data", "carbs": "No Data", "protein": "No Data", "fat": "No Data", "date_time": "No Data"}]
     })
 
 @app.route('/diet/<int:traineeid>/monthly')

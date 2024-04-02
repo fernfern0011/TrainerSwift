0c59d2db9cadcc4aa3f401af4c411de20dc0f464
DylanChua2001 <dylanchua2001dc@gmail.com>
2024-04-02 20:37:59 +0800

    calculator session done

--- frontend/src/app/(trainer)/client-diet/page.js
+++ frontend/src/app/(trainer)/client-diet/page.js
@@ -2,16 +2,38 @@
 import { Heading, Spacer, Box, Flex, Button, Menu, MenuButton, MenuList, MenuItem, TableContainer, Table, Thead, Tr, Th, Td, Tbody, Tfoot, Stack } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { React, useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 export default function DietPage() {
     const [meals, setMeals] = useState([]);
     const [calcData, setCalcData] = useState([]);
     const [type, setType] = useState('bulk');
+    const [checkToken, setCheckToken] = useState('')
 
     useEffect(() => {
+        const token = Cookies.get('token')
+        const trainerinfo = Cookies.get('trainerinfo')
+        var trainerid
+
+        if (!token) {
+            router.replace('/') // If no token is found, redirect to login page
+            return
+        }
+
+        if (!(trainerinfo === undefined)) {
+            trainerid = JSON.parse(trainerinfo)
+        }
+
+        setCheckToken(token)
         const fetchMeals = async () => {
             try {
-                const response = await fetch('http://localhost:3000/api/diet/7');
+                const response = await fetch('http://localhost:3000/api/diet/7', {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`,
+                    }
+                });
                 if (!response.ok) {
                     throw new Error('Failed to fetch meals');
                 }
@@ -52,16 +74,16 @@ export default function DietPage() {
                     <Heading mt={20} mb={10} size='3xl'>Trainee 7 Diet</Heading>
                 </Box>
                 <Box bg="gray.200" p={4} ml="auto" mr={12} borderRadius="md">
-                <Flex alignItems="center">
-                    <Heading size="md" ml={5}>This Month</Heading>
-                    <Spacer />
-                    <Button colorScheme="teal" variant="outline" mr={4} onClick={() => handleTypeSelect('bulk')}>
-                        Bulk
-                    </Button>
+                    <Flex alignItems="center">
+                        <Heading size="md" ml={5}>This Month</Heading>
+                        <Spacer />
+                        <Button colorScheme="teal" variant="outline" mr={4} onClick={() => handleTypeSelect('bulk')}>
+                            Bulk
+                        </Button>
                         <Button colorScheme="teal" variant="outline" onClick={() => handleTypeSelect('cut')}>
-                        Cut
-                    </Button>
-                </Flex>
+                            Cut
+                        </Button>
+                    </Flex>
                     <TableContainer>
                         <Table variant='simple'>
                             <Thead>
@@ -77,8 +99,8 @@ export default function DietPage() {
                                     <Tr key={index}>
                                         <Td>{item.nutrients}</Td>
                                         <Td isNumeric>{item.current}</Td>
-                                        <Td isNumeric>{ type == 'bulk' ? item.target.bulk : item.target.cut}</Td>
-                                        <Td>{ type == 'bulk' ? item.diff.bulk : item.diff.cut }</Td>
+                                        <Td isNumeric>{type == 'bulk' ? item.target.bulk : item.target.cut}</Td>
+                                        <Td>{type == 'bulk' ? item.diff.bulk : item.diff.cut}</Td>
                                     </Tr>
                                 ))}
                             </Tbody>
--- frontend/src/app/api/calculator/[traineeid]/route.js
+++ frontend/src/app/api/calculator/[traineeid]/route.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken'
+import { headers } from 'next/headers'
 import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL = 'http://localhost:8000/calculatorapi/calculator/'
@@ -6,9 +8,25 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(req, context) {
     const { traineeid } = await context.params
+    try {
+        const headersInstance = headers()
+        const authHeader = headersInstance.get('Authorization')
 
-    const res = await fetch(`${DATA_SOURCE_URL}/${traineeid}`)
-    const getCalcResult = await res.json()
+        const token = authHeader.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    return NextResponse.json(getCalcResult)
+        if (!decoded) {
+            return NextResponse.json({ "code": 400, "message": "Expired" })
+        } else if (decoded.exp < Math.floor(Date.now() / 1000)) {
+            return NextResponse.json({ "code": 400, "message": "Expired" })
+        } else {
+            const res = await fetch(`${DATA_SOURCE_URL}/${traineeid}`)
+            const getCalcResult = await res.json()
+
+            return NextResponse.json(getCalcResult)
+        }
+    } catch (error) {
+        console.error('Token verification failed', error)
+        return NextResponse.json({ "code": 400, "message": "Unauthorized" })
+    }
 }

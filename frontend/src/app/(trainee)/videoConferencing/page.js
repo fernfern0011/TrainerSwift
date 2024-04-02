6fc0a0e70755255ed8f1764cc5975ec1f8e8f559
theMarcokuok <marcokuok6@gmail.com>
2024-04-03 00:31:00 +0800

    partially added session for videoconferencing, line70

--- frontend/src/app/(trainee)/videoConferencing/page.js
+++ frontend/src/app/(trainee)/videoConferencing/page.js
@@ -12,20 +12,69 @@ import {
 } from '@livekit/components-react';
 import { Track } from 'livekit-client';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 
 export default function Page() {
   // TODO: get user input for room and name
   const room = "test";
-  const name = "chris poskitt";
   const [token, setToken] = useState("");
+  const [checkToken, setCheckToken] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
+    const token = Cookies.get('token')
+    const traineeinfo = Cookies.get('traineeinfo')
+    var traineeid
+    var bookedby
+
+    if (!token) {
+      router.replace('/') // If no token is found, redirect to login page
+      return
+    }
+
+    if (!(traineeinfo === undefined)) {
+      traineeid = JSON.parse(traineeinfo)
+    }
+
+    setCheckToken(token)
+    const fetchBookedBy = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/trainee-booking/${traineeid.traineeid}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+          }
+        });
+        console.log(response)
+        if (!response.ok) {
+          throw new Error('Failed to fetch trainer information');
+        }
+        const data = await response.json();
+        console.log(data)
+        bookedby = data.data.bookedby[0].bookedbyid;
+        console.log(bookedby)
+
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchBookedBy();
+
     (async () => {
       try {
         const resp = await fetch(
-          `http://localhost:3000/api/get-participant-token?room=${room}&username=${name}`
-        );
+          `http://localhost:3000/api/get-participant-token?room=${bookedby}&username=${traineeid.traineeid}`
+          , {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`,
+            }
+          });
         const data = await resp.json();
         setToken(data.token);
       } catch (e) {
